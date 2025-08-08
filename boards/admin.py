73c0d5285beca904701d1ada90ed6e5dd1019fc9
Евгений Сульжицкий <idholleyyt@vk.com>
2025-08-08 21:25:52 +0300

    Регистрация моделей в админке.

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from boards.models import Board, BoardMember
+
+
+class BoardMemberAdmin(admin.ModelAdmin):
+    list_display = ('id', 'board', 'user', 'role', 'added_at')
+    list_filter = ('role', 'added_at')
+    search_fields = ('board__title', 'user__username')
+    list_select_related = ('board', 'user')
+
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at', 'owner')
+    list_editable = ('title',)
+    search_fields = ('title', 'description', 'owner__username')
+    list_select_related = ('owner',)
+
+
+admin.site.register(Board, BoardAdmin)
+admin.site.register(BoardMember, BoardMemberAdmin)
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'board', 'status', 'assignee', 'creator', 'created_at'
+    )
+    list_filter = ('status', 'board', 'assignee', 'creator', 'created_at')
+    search_fields = ('title', 'description', 'board__title')
+    list_editable = ('status', 'assignee')
+    list_select_related = ('board', 'assignee', 'creator')
+
+
+admin.site.register(Task, TaskAdmin)
